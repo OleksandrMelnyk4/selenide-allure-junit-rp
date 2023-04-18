ba4cc9741fc775216ff2457904ebe5369a0e4d77
Oleksandr Melnyk <oleksandr_melnyk4@epam.com>
2023-04-18 15:42:20 +0200

    add assertion to test

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -4,12 +4,16 @@ import buisness.DashboardPage;
 import buisness.FiltersPage;
 import buisness.LoginPage;
 import buisness.components.NavigationMenuComponent;
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Description;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static core.enums.UsersRole.ADMINISTRATOR;
 import static core.utils.constants.MenuNames.FILTERS;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@Slf4j
 class Tests extends BaseTest {
 
   LoginPage login = new LoginPage();
@@ -24,6 +28,9 @@ class Tests extends BaseTest {
     dashboardPage.dashboardPageShouldBeOpened();
     navigationMenuComponent.navigateToMenu(FILTERS);
     filtersPage.filtersPageShouldBeOpened();
-  }
 
+    String session = Selenide.sessionId().toString();
+    assertFalse(session.isEmpty());
+    log.info("Test passed and session is %s".formatted(session));
+  }
 }
