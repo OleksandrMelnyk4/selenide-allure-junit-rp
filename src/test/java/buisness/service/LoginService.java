f00d3a5912178f47e8e26ec9469e54b422d679e5
Oleksandr Melnyk <oleksandr_melnyk4@epam.com>
2023-05-16 16:57:04 +0200

    fix comments

--- src/test/java/buisness/service/LoginService.java
+++ src/test/java/buisness/service/LoginService.java
@@ -5,9 +5,10 @@ import buisness.core.LoginPage;
 import com.codeborne.selenide.Selenide;
 import core.dto.LoginUserDto;
 import core.enums.UsersRole;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.slf4j.Slf4j;
 
-import static core.enums.PropertyNames.BASE_URL;
+import static com.codeborne.selenide.Selenide.open;
 import static core.utils.services.ConfigurationService.getProperty;
 
 @Slf4j
@@ -16,7 +17,9 @@ public class LoginService {
   private final DashboardPage dashboardPage = new DashboardPage();
 
   private void openLoginPage() {
-    Selenide.open(getProperty(BASE_URL.getName()));
+    WebDriverManager.chromedriver().setup();
+    open("/");
+    Selenide.webdriver().driver().getWebDriver().manage().window().maximize();
   }
 
   private LoginUserDto getUser(String userRole) {
--- src/test/java/stepdefs/CucumberHook.java
+++ src/test/java/stepdefs/CucumberHook.java
@@ -1,11 +1,8 @@
 package stepdefs;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.OutputType;
 
@@ -13,18 +10,6 @@ import java.util.Base64;
 
 @Slf4j
 public class CucumberHook {
-
-  @Before
-  public void beforeScenario(Scenario scenario) {
-    WebDriverManager.chromedriver().setup();
-
-    Configuration.browser = "chrome";
-    Configuration.driverManagerEnabled = true;
-    Configuration.browserSize = "1920x1080";
-    Configuration.headless = false;
-    Configuration.webdriverLogsEnabled = true;
-    log.info("START SCENARIO: " + scenario.getName());
-  }
 
   @After
   public void afterScenario(Scenario scenario) {
